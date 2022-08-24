daa9f663cc3c1299acdcc015aea791da988012d7
sikander-afzaal <sikander.aaa@gmail.com>
2022-08-24 13:02:24 +0500

    home page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Footer from "./Layout/Footer/Footer";
 import Header from "./Layout/Header/Header";
-
+import Home from "./Pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route element={<Home />} path="/" />
+      </Routes>
       <Footer />
     </div>
   );
